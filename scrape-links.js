a46f2292b311f5a762839e5437079f48e7eb7f72
Ryan Roemer <ryan@loose-bits.com>
2016-09-28 02:50:15 -0700

    Switch to md parsing instead of regex. Enable lint for root js files.

--- scrape-links.js
+++ scrape-links.js
@@ -1,26 +1,38 @@
 "use strict";
+
 var path = require("path");
 var fs = require("fs");
-var PATH = "./node_modules/formidable-playbook";
-var OUTPUT = "./links.json";
+var md = require("markdown-it")();
+
+var PATH = path.dirname(require.resolve("formidable-playbook/package.json"));
 var README = path.join(PATH, "README.md");
-var REGEX = /\[(.*)\]\((?!http)(?!#)(.*)\)/gm;
+var LINKS = "./links.json";
 
-fs.readFile(README, "utf8", function (error, data) {
-  if (error) {
-    throw error;
-  }
+fs.readFile(README, "utf8", function (readErr, data) {
+  if (readErr) { throw readErr; }
 
-  var links = [];
-  var result = REGEX.exec(data);
-  while(result) {
-    links.push({
-      label: result[1],
-      route: "/" + result[2].split(".md")[0],
+  var links = md.parse(data)
+    // Convert to useful info for us for top-level links.
+    .map(function (token) {
+      // Look for children of size three (link open, text, link close).
+      var children = token.children || [];
+      if (children.length !== 3) { return {}; } // eslint-disable-line no-magic-numbers
+
+      // Grab the route from href attr.
+      var route = ((children[0].attrs || [])[0] || [])[1] || "";
+
+      // Convert to final form for links.
+      return {
+        label: (children[1] || []).content,
+        route: "/" + route.split(".md")[0]
+      };
+    })
+    // Limit to top-level links that start with `docs/`
+    .filter(function (obj) {
+      return (obj.route || "").indexOf("/docs/") === 0;
     });
 
-    result = REGEX.exec(data);
-  }
-
-  fs.writeFile(OUTPUT, JSON.stringify(links));
+  fs.writeFile(LINKS, JSON.stringify(links, null, 2), function (writeErr) {
+    if (writeErr) { throw writeErr; }
+  });
 });
